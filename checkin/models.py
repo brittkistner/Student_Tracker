50369ccf1f07707021903f953d27ba70d0e02e9e
Brittney Kistner <bkistner01@gmail.com>
2014-10-14 16:16:40 -0700

    Models Added

--- checkin/models.py
+++ checkin/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Teacher(AbstractUser):
+    def __unicode__(self):
+        return self.first_name
+
+
+class Student(AbstractUser):
+    def __unicode__(self):
+        return self.first_name
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher, related_name="classes")
+    student = models.ManyToManyField(Student, related_name="classes")
+    class_start = models.DateTimeField()
+    class_end = models.DateTimeField()
+
+
+class CheckIn(models.Model):
+    student = models.ForeignKey(Student, related_name="check_ins")
+    class_name = models.ForeignKey(Class, related_name="check_ins")
+    check_in_time = models.DateTimeField(auto_now_add=True)
+
+
+class HelpMe(models.Model):
+    student = models.ForeignKey(Student, related_name="help_mes")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
