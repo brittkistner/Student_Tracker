e477e12d8393293a5bc2d26fd371597e2052b023
PAULA BUESO-INCHAUSTI <paulabueso-inchausti@MacBook-Air-de-PAULA-BUESO-INCHAUSTI.local>
2014-10-15 07:52:38 -0700

    added checkin functionality and home view

--- checkin/views.py
+++ checkin/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from checkin.forms import EmailUserCreationForm
+from django.shortcuts import render_to_response
+from checkin.forms import EmailUserCreationForm, StudentCheckInForm
+from checkin.models import CheckIn, Class
+
 
 # ##############
 # REGISTRATION #
@@ -33,8 +37,50 @@ def register(request):
 # HOME #
 ########
 
+@login_required()
 def home(request):
+    if not request.user.is_student:
+        classes = Class.objects.filter(teacher=request.user)
+    else:
+        classes = Class.objects.filter(student=request.user)
     data = {
-        'user': request.user,
+        'classes': classes
     }
     return render(request, 'home.html', data)
+
+
+# check in the students to class
+# check in from request Post
+@login_required()
+def checkin(request):
+    if request.user.is_student:
+    #Check if student or teacher
+        student_check_in_form = StudentCheckInForm(student=request.user)
+        #Pass in student user to get classes for the particular student in form
+        if request.method == "POST":
+            student_check_in_form = StudentCheckInForm(request.POST)
+            if student_check_in_form.is_valid():
+                checkin = CheckIn.objects.create(
+                    student=request.user,
+                    class_name=Class.objects.get(
+                        pk=int(student_check_in_form.cleaned_data['classes'])
+                    )
+                )
+                if checkin:
+                    return redirect('home')
+
+    data = {'student_check_in_form': student_check_in_form}
+    return render(request, 'checkin/checkin.html', data)
+
+# class Class(models.Model):
+#     name = models.CharField(max_length=50)
+#     teacher = models.ForeignKey(UserProfile, related_name="classes_teacher")
+#     student = models.ManyToManyField(UserProfile, related_name="classes_student")
+#     class_start = models.DateTimeField()
+#     class_end = models.DateTimeField()
+#
+#
+# class CheckIn(models.Model):
+#     student = models.ForeignKey(UserProfile, related_name="check_ins")
+#     class_name = models.ForeignKey(Class, related_name="check_ins")
+#     check_in_time = models.DateTimeField(auto_now_add=True)
