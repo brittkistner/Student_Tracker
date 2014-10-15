14afb69b764e2b7a800510d94ba32aa09225ee3c
Peter <pwgraham91@gmail.com>
2014-10-14 23:16:28 -0700

    helpme works with working timer

--- checkin/static/js/homeHelpMe.js
+++ checkin/static/js/homeHelpMe.js
@@ -2,12 +2,16 @@
  * Created by GoldenGate on 10/14/14.
  */
 $(document).ready(function() {
-    $.ajax({
-        url: 'helpMe/',
-        type: 'GET',
-        success: function (data) {
-            $('.helpMe').html(data);
-        }
-    });
-//    $('#addMeButton').onclick(function)
+
+    function loadHelp () {
+        $.ajax({
+            url: 'helpMe/',
+            type: 'GET',
+            success: function (data) {
+                $('.helpMe').html(data);
+            }
+        });
+    }
+loadHelp();
+    setInterval(loadHelp, 1000)
 });
